b70bd7fb169eacd1ca8a3c7e7a347f9a1a05a3f1
Azhar Ali Khaked <gr33nmayhem@gmail.com>
2018-01-30 18:57:29 +0530

    Replaced NRActivity with XActivity
    A more generic name given to the NRActivity and also the activity has been changed to work with sugar 1.2 now 
    
    ran autopep8 on the code to fix any wrong format

---
+++ XActivity.py
@@ -0,0 +1,86 @@
+# XActivity.py
+#
+# Copyright (C) 2018 Azhar Ali Khaked here
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software
+# Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General
+# Public License along with this program; if not, write
+# to the Free Software Foundation, Inc., 51 Franklin
+# St, Fifth Floor, Boston, MA 02110-1301  USA
+from gettext import gettext as _
+
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import pygame
+
+from sugar3.activity.activity import Activity
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.graphics.toolbutton import ToolButton
+from sugar3.activity.widgets import StopButton
+
+
+sys.path.append('..')  # Import sugargame package from top directory.
+import sugargame.canvas
+
+import NumRush
+
+
+class XActivity(Activity):
+    def __init__(self, handle):
+        Activity.__init__(self,handle)
+
+        # Create the game instance.
+        self.game = NumRush.numrush()
+
+        # Build the activity toolbar.
+        self.build_toolbar()
+
+        # Build the Pygame canvas and start the game running
+        # (self.game.run is called when the activity constructor
+        # returns).
+        self._pygamecanvas = sugargame.canvas.PygameCanvas(self,
+            main=self.game.run, modules=[pygame.display, pygame.font])
+
+        # Note that set_canvas implicitly calls read_file when
+        # resuming from the Journal.
+        self.set_canvas(self._pygamecanvas)
+        self._pygamecanvas.grab_focus()
+
+    def build_toolbar(self):
+        toolbar_box = ToolbarBox()
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show()
+
+        activity_button = ActivityToolbarButton(self)
+        toolbar_box.toolbar.insert(activity_button, -1)
+        activity_button.show()
+
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(True)
+        toolbar_box.toolbar.insert(separator, -1)
+        separator.show()
+
+        stop_button = StopButton(self)
+        toolbar_box.toolbar.insert(stop_button, -1)
+        stop_button.show()
+
+    def read_file(self, file_path):
+        pass  # self.game.read_file(file_path)
+
+    def write_file(self, file_path):
+        pass  # self.game.write_file(file_path)
