bc5a55152ff31bcec36c6ab682e118cd6f73d2a5
sourabhaa <sourabhayoga@gmail.com>
2022-04-08 01:18:10 +0530

    fix: refactored code; store and resore high score between instances

--- NumRush.py
+++ NumRush.py
@@ -372,7 +372,7 @@ class numrush():
 
     def save_game(self):
         ''' Return game state for saving to Journal '''
-        return  hscore, score
+        return hscore, score
 
     def read_highscore(self):
         highscore = [0]
@@ -397,6 +397,7 @@ class numrush():
             logging.exception(e)
             return 0
         return 0
+
 
 if __name__ == '__main__':
     main()
